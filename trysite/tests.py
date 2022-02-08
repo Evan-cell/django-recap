7851c93b7f5b83f9d23940ed1465e1446ad0167d
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-08 10:24:05 +0300

    tests'

--- trysite/tests.py
+++ trysite/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Article,Editor,tags
 # Create your tests here.
+class EditorTestClass(TestCase):
+    def setUp(self):
+        self.james = Editor(first_name = 'james', last_name = 'mwakio', email = 'mwakio@gmail.com')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james,Editor)) 
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+    def test_delete_method(self):
+        self.james.delete_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)<1)           
