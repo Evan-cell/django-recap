21427d7cd1a93d53571f243bb03b30fbd3a49a62
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-08 09:26:18 +0300

    added article and tag models

--- trysite/models.py
+++ trysite/models.py
@@ -15,6 +15,8 @@ class tags(models.Model):
     def __str__(self):
         return self.name
 class Article(models.Model):
-    title = models.CharField(max_length=30)
-    post = models.TextField(max_length=60)
-    editor = models.ForeignKey(Editor)    
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)   
