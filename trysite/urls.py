4bd9d7ce7ae52cbd881e1665da2912740af2e42d
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-02-08 10:34:53 +0300

    phone_number feild'

--- trysite/urls.py
+++ trysite/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
     path('now/',views.current_date, name="date"),
-    path('welcome/', views.welcome, name="welcome")
+    path('', views.welcome, name="welcome")
 ]
